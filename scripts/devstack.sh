e52c70a4cd87a31042d2fabdfeeac8cbf36c1606
Uprise Software <uprisesoft@gmail.com>
2017-10-01 11:49:21 +0200

    Fixed folder structure problem

--- scripts/devstack.sh
+++ scripts/devstack.sh
@@ -1,17 +1,17 @@
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 
-sudo su - stack -c "git clone -b stable/pike https://github.com/rmaire/devstack.git /opt/stack/devstack"
+sudo su - stack -c "git clone -b stable/pike https://github.com/rmaire/devstack.git"
 
-sudo su - stack -c "echo [[local\|localrc]] > /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo ADMIN_PASSWORD=$3 >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo DATABASE_PASSWORD=$3 >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo RABBIT_PASSWORD=$3 >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo SERVICE_PASSWORD=$3 >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo HOST_IP=$1 >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo COMPRESS_OFFLINE=false >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo COMPRESS_ENABLED=false >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo disable_service tempest >> /opt/stack/devstack/local.conf"
-sudo su - stack -c "echo FLOATING_RANGE=$2 >> /opt/stack/devstack/local.conf"
+sudo su - stack -c "echo [[local\|localrc]] > devstack/local.conf"
+sudo su - stack -c "echo ADMIN_PASSWORD=$3 >> devstack/local.conf"
+sudo su - stack -c "echo DATABASE_PASSWORD=$3 >> devstack/local.conf"
+sudo su - stack -c "echo RABBIT_PASSWORD=$3 >> devstack/local.conf"
+sudo su - stack -c "echo SERVICE_PASSWORD=$3 >> devstack/local.conf"
+sudo su - stack -c "echo HOST_IP=$1 >> devstack/local.conf"
+sudo su - stack -c "echo COMPRESS_OFFLINE=false >> devstack/local.conf"
+sudo su - stack -c "echo COMPRESS_ENABLED=false >> devstack/local.conf"
+sudo su - stack -c "echo disable_service tempest >> devstack/local.conf"
+sudo su - stack -c "echo FLOATING_RANGE=$2 >> devstack/local.conf"
 
-sudo su - stack -c "cd /opt/stack/devstack; ./stack.sh"
+sudo su - stack -c "cd devstack; ./stack.sh"
