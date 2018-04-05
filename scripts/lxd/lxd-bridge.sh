056f5790f705f13f2184918a61b3c8636648c17f
Roman Maire <roman.maire@zuara.ch>
2018-04-05 11:35:58 +0200

    LXD Fit try

--- scripts/lxd/lxd-bridge.sh
+++ scripts/lxd/lxd-bridge.sh
@@ -4,12 +4,12 @@ ufw disable
 
 cat <<EOF >> /etc/network/interfaces
 
-auto enp0s9
-iface enp0s9 inet manual
+auto eth2
+iface eth2 inet manual
 
 auto br0
 iface br0 inet dhcp
-	bridge_ports enp0s9
+	bridge_ports eth2
 	bridge_stp off
 	bridge_maxwait 0
 EOF
